4c0fbae32979735f19f56d215f20e43508462209
wookdong <wook5307@gmail.com>
2024-01-08 22:01:46 +0900

    test1

---
+++ src/example/Dog.java
@@ -0,0 +1,14 @@
+package example;
+
+public class Dog extends Animal{
+    public Dog(String name, int age){
+        super(name, age);
+    }
+
+    @Override
+    public void bark() {
+        System.out.println("멍멍");
+    }
+
+
+}
