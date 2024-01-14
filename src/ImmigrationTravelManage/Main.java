539b1d69b20c00952f3af41efe44d514b6f156f8
wookd0ng <wook5307@gmail.com>
2024-01-14 23:14:58 +0900

    Update ImmigrationTravelManage/Main.java

--- src/ImmigrationTravelManage/Main.java
+++ src/ImmigrationTravelManage/Main.java
@@ -1,32 +1,81 @@
 package ImmigrationTravelManage;
 
-import javax.imageio.plugins.jpeg.JPEGImageReadParam;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
-        //이런식으로 이민이랑 여행 서비스 넣어야댐
-        PersonStruct person1 = new PersonStruct("홍길동",30,"남","한국",1,"KOREA");
+        //personcoin 1,2는 각각 잉글랜드 여행, 한국 여행이고(위치만 변경됨) 3,4는 잉글랜드 이민, 한국 이민입니다(국적,위치 변경됨).
+        //입국 제한 대상국은 North Korea. 철자 주의. 대문자 포함됨.
+        //단체 입국 인원은 11명이상입니다.
+        //망명 신청은 스탬프란에 SOS라고 입력하면 됩니다. 모두 대문자.
+        PersonStruct person1 = new PersonStruct("홍길동", 30, "남", "KOREA", 3, "KOREA",12,"SOS");
         CountryStruct korea = new CountryStruct("KOREA", 50000000, "SEOUL", "ASIA");
-        CountryStruct england = new CountryStruct("ENGLAND", 56000000, "LONDON", "EUROPE");
 
-        System.out.println("1.여행 서비스\n2.이민서비스");
-        System.out.println("신원조회 시작하겠습니다" + "이름 :" + person1.personname+"\n"+
-                                                 "나이 :"+person1.personage+"\n"+
-                                                "성별 :"+person1.persongender+"\n"+
-                                                "국적 :"+person1.personcountry+"\n"+
-                                                "현위치 :"+person1.personlocation);
+        System.out.println("-----신원조회 시작하겠습니다-----\n" +
+                "이름: " + person1.personname + "\n" +
+                "나이: " + person1.personage + "\n" +
+                "성별: " + person1.persongender + "\n" +
+                "국적: " + person1.personcountry + "\n" +
+                "현위치: " + person1.personlocation + "\n----------");
+
+        //person1.getPersoncoin(); -> 여행,이민을 가시는 군요 return문이 나와서 필요가 없어짐.
+        //String currentCountry = person1.getPersonCountry();
+        if ("North Korea".equals(person1.personcountry)) {
+            System.out.println("입국제한입니다.");
+            System.out.println("----------");
+
+            return;
+        } else {
+            System.out.println("입국제한 대상자가 아닙니다.");
+            System.out.println("----------");
+
+        }
+        TravelManger travelManger = new TravelManger(); //구현 클래스 호출
+        ImmigrationManager immigrationManager = new ImmigrationManager(); //구현 클래스 호출
 
 
+        if(person1.personcoin == 1){
+            System.out.println("여행 가시는 군요?");
+            travelManger.changeLocation(person1 ,"ENGLAND");
+        }
+
+        if(person1.personcoin == 2){
+            System.out.println("여행 가시는 군요?");
+            travelManger.changeLocation(person1 ,"KOREA");
+        }
 
 
+        if(person1.personcoin == 3){
+            System.out.println("이민 가시는 군요?");
+            immigrationManager.changeCountry(person1 ,"ENGLAND");
+            travelManger.changeLocation(person1 ,"ENGLAND");
 
-        //여행관리 인터페이스랑 국적 관리 인터페이스
-        //여행 관리는 위치만 바꾸고 국적관리는 나라 바꾸기
-        System.out.println("지금 방문하시는 나라는 " + korea.countryName + "입니다.\n" + "인구수는 " + korea.populaTion + "이고, 속한 대륙은 " + korea.continent + ", 수도는 " + korea.capital + "입니다.\n");
+        }
+        else if(person1.personcoin == 4){
+            System.out.println("이민 가시는 군요?");
+            immigrationManager.changeCountry(person1 ,"KOREA");
+            travelManger.changeLocation(person1 ,"KOREA");
 
+        }
 
-        System.out.println("지금 방문하시는 나라는 " + england.countryName + "입니다.\n" + "인구수는 " + england.populaTion + "이고, 속한 대륙은 " + england.continent + ", 수도는 " + england.capital + "입니다.\n");
+        if(person1.member>10){
+            System.out.println("단체 입국 해당자입니다.");
+            System.out.println("----------");
 
+        }
+        else {
+            System.out.println("단체 입국 해당자가 아닙니다.");
+            System.out.println("----------");
+
+        }
+        if(Objects.equals(person1.stamp, "SOS")){
+            System.out.println("망명신청자입니다. 각국 대사관으로 문의하세요.");
+            System.out.println("----------");
+
+        }
+        else {System.out.println("망명 해당자가 아닙니다.");}
+        System.out.println("----------");
 
     }
 }
+
