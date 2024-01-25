fd6eadd9f3f6bdcb9eaca6aaeefa814e500c54e4
wookd0ng <wook5307@gmail.com>
2024-01-25 21:35:52 +0900

    LOTTO_PROGRAM

---
+++ src/TravelSystem/AllinterFace.java
@@ -0,0 +1,37 @@
+package TravelSystem;
+
+public abstract class AllinterFace {
+
+
+
+    public void count(KoreaCitizen koreaCitizen) {
+
+    }
+
+
+    public void location(KoreaCitizen koreaCitizen) {
+
+    }
+
+
+    public void citizennum(KoreaCitizen koreaCitizen) {
+
+    }
+
+
+    public void immigration() {
+
+    }
+
+
+    public void citizennum() {
+
+    }
+
+
+    public void license() {
+
+    }
+
+    public abstract void immigration(KoreaCitizen koreaCitizen);
+}
