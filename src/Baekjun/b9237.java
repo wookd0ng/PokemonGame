2a83b389dab5fa37762371af304341fbeeccf061
wookd0ng <wook5307@gmail.com>
2024-02-01 23:45:12 +0900

    [푸는중]백준 알고리즘 9237 문제 푸는중

---
+++ src/Baekjun/b9237.java
@@ -0,0 +1,25 @@
+package Baekjun;
+
+import java.util.Scanner;
+
+public class b9237 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=0; //묘목의 개수
+        n= sc.nextInt(); //묘목의 갯수를 첫째줄에 입력받음
+
+        //각 묘목이 다 자라는데 걸리는 일수. 정렬
+        //한개 심는데 하루 소요
+        //답이 여러 가지인 경우에는 가장 작은 값 출력
+        /*
+        4
+        2 3 4 3
+
+        결과값 ==7
+
+        묘목의 개수는 4.
+
+         */
+
+    }
+}
